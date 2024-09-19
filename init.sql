8381d95192811b9ad0b00611e3e1d7a44e451f87
Tinchocw <mcwikla@fi.uba.ar>
2024-09-19 14:49:25 -0300

    Add new admin table when initializing docker

--- init.sql
+++ init.sql
@@ -7,6 +7,7 @@ $do$
 BEGIN
    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'twitsnap_users') THEN
       CREATE DATABASE twitsnap_users;
+
    END IF;
 END
 $do$;
@@ -18,6 +19,14 @@ $do$;
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the users table if it doesn't exist
+CREATE TABLE IF NOT EXISTS admins (
+    username VARCHAR(50) PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
