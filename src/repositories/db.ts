685af0c088fb2951a6d78216592a239d271e6309
Luis Escalante <lescalante@fi.uba.ar>
2024-09-19 21:12:09 -0300

    add log

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function createPool() {
+  console.log('Creating a new database pool', process.env.DATABASE_URL);
   return new Pool({
     connectionString: process.env.DATABASE_URL,
   });
