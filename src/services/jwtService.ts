4f61a1c257e93ae19fc311164f640cbae89a2736
Luis Escalante <lescalante@fi.uba.ar>
2024-09-21 18:41:03 -0300

    create jwt service

---
+++ src/services/jwtService.ts
@@ -0,0 +1,26 @@
+import { IJWTService, JwtCustomPayload } from 'auth';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+// JWT Service class implementation
+export class JWTService implements IJWTService {
+
+    readonly expiresIn = '365 days';
+    readonly secret = process.env.JWT_SECRET_KEY!;
+    
+
+  sign(payload: JwtCustomPayload): string {
+    return jwt.sign(payload, this.secret, { expiresIn: this.expiresIn });
+  }
+
+  verify(token: string): JwtPayload | string {
+    try {
+      return jwt.verify(token, this.secret);
+    } catch (error) {
+      throw new Error('Invalid token');
+    }
+  }
+
+  decode(token: string): JwtPayload | string | null {
+    return jwt.decode(token);
+  }
+}
---
+++ src/types/auth.ts
@@ -0,0 +1,24 @@
+import { JwtPayload } from "jsonwebtoken";
+
+// Interface definition
+export interface IJWTService {
+  sign(payload: JwtCustomPayload): string;
+  verify(token: string): JwtPayload | string;
+  decode(token: string): JwtPayload | string | null;
+}
+
+
+export type JwtCustomPayload = JwtUserPayload | JwtAdminPayload;
+
+export type JwtUserPayload = {
+    type: 'user';
+    userId: number;
+    email: string;
+    username: string;
+}
+
+export type JwtAdminPayload = {
+    type: 'admin';
+    adminId: number;
+    email: string;
+}
