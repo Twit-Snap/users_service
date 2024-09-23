c3acc39908cd6a5805bffab529b2afafc5d47001
Tinchocw <mcwikla@fi.uba.ar>
2024-09-22 22:05:00 -0300

    Add lint

--- src/types/admin.ts
+++ src/types/admin.ts
@@ -1,19 +1,17 @@
 export interface Admin {
-    username: string;
-    email: string;
+  username: string;
+  email: string;
 }
 
 export type adminWithToken = Admin & { token: string };
 
 export interface AdminInfoDto {
-    username: string;
-    email: string;
-    password: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
 export interface LoginAdminDto {
   email: string;
   password: string;
 }
-
-
