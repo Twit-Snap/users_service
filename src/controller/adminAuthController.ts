6f310fa43b3391c53d0905172aa4912c722b5e79
Tinchocw <mcwikla@fi.uba.ar>
2024-09-24 15:11:43 -0300

    Ref: Change error message

--- src/controller/adminAuthController.ts
+++ src/controller/adminAuthController.ts
@@ -37,7 +37,7 @@ export class AdminAuthController {
     }
 
     if (!req.body.email && !req.body.username) {
-      throw new ValidationError('email or username', 'Email or username are required','INVALID EMAIL OR USERNAME');
+      throw new ValidationError('email/username', 'Email/username is required','INVALID EMAIL/USERNAME');
     }
   }
 
