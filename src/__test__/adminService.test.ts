d209120dd36b417d3c92acb6faa10b718f08037c
Tinchocw <mcwikla@fi.uba.ar>
2024-09-25 15:23:49 -0300

    Add/Ref: adminService and adminAuthService tests and new of constructor admin service

---
+++ src/__test__/adminService.test.ts
@@ -0,0 +1,71 @@
+import { AdminService } from '../services/adminService';
+import { NotFoundError } from '../types/customErrors';
+import { UserRepository } from '../repositories/userRepository';
+import { User } from 'user';
+
+
+jest.mock('../repositories/adminRepository');
+jest.mock('../services/jwtService');
+jest.mock('bcrypt');
+
+describe('AdminService', () => {
+  let service: AdminService;
+  let dbServiceMock: jest.Mocked<UserRepository>;
+
+  const aMockUser: User = {
+    id: 1,
+    username: 'aUser',
+    email: 'aUser@example.com',
+    name: 'aUser',
+    lastname: 'aUserLastName',
+    birthdate: new Date(),
+    createdAt: new Date(),
+  }
+
+  const anotherMockUser: User = {
+    id: 1,
+    username: 'anotherUser',
+    email: 'anotherUser@example.com',
+    name: 'anotherUser',
+    lastname: 'anotherUserLastName',
+    birthdate: new Date(),
+    createdAt: new Date(),
+  }
+
+  const mockUsers = [aMockUser, anotherMockUser];
+
+  beforeEach(() => {
+
+    dbServiceMock = {
+      getList: jest.fn().mockResolvedValue(mockUsers),
+      getByUsername: jest.fn().mockResolvedValue(aMockUser),
+    } as unknown as jest.Mocked<UserRepository>;
+
+    service = new AdminService(dbServiceMock);
+
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getUserList', () => {
+    it('should return a list of users', async () => {
+      const result = await service.getUserList();
+      expect(result).toEqual(mockUsers);
+    });
+  });
+
+  describe('getUserByUsername', () => {
+    it('should return a user', async () => {
+      const result = await service.getUserByUsername(aMockUser.username);
+      expect(result).toEqual(aMockUser);
+    });
+
+    it('should throw an error if user is not found', async () => {
+      dbServiceMock.getByUsername.mockResolvedValue(null);
+      await expect(service.getUserByUsername('nonExistentUser')).rejects.toThrow(NotFoundError);
+    });
+  });
+
+});
