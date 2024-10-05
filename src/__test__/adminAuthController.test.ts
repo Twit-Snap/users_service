c62923322af048d354981628593cbe7177345af0
Tinchocw <mcwikla@fi.uba.ar>
2024-10-05 15:55:24 -0300

    Refactor: Change structure of tests

--- src/__test__/adminAuthController.test.ts
+++ src/__test__/adminAuthController.test.ts
@@ -14,167 +14,173 @@ describe('AdminAuthController', () => {
     controller = new AdminAuthController();
   });
 
-  it('should create an admin successfully', async () => {
-    const adminData = {username: 'admin1', email: 'admin1@example.com' };
-    (AdminAuthService.prototype.createAdmin as jest.Mock).mockResolvedValue(adminData);
+  describe('CreateAdmin', () => {
 
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        username: 'admin1',
-        email: 'admin1@example.com',
-        password: 'securepassword',
-      },
+    it('should create an admin successfully', async () => {
+      const adminData = { username: 'admin1', email: 'admin1@example.com' };
+      (AdminAuthService.prototype.createAdmin as jest.Mock).mockResolvedValue(adminData);
+
+      const req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/admin/register',
+        body: {
+          username: 'admin1',
+          email: 'admin1@example.com',
+          password: 'securepassword',
+        },
+      });
+
+      const response = await controller.createAdmin(req);
+
+      expect(response).toEqual({ data: adminData });
+      expect(AdminAuthService.prototype.createAdmin).toHaveBeenCalledWith(req.body);
     });
 
-    const response = await controller.createAdmin(req);
+    it('create an admin should throw an error if username is empty', async () => {
+      const req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/admin/register',
+        body: {
+          username: '',
+          email: 'admin1@example.com',
+          password: 'securepassword',
+        },
+      });
 
-    expect(response).toEqual({ data: adminData });
-    expect(AdminAuthService.prototype.createAdmin).toHaveBeenCalledWith(req.body);
-  });
-
-  it('create an admin should throw an error if username is empty', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        username: '',
-        email: 'admin1@example.com',
-        password: 'securepassword',
-      },
+      await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
     });
 
-    await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
-  });
+    it('create an admin should throw an error if username is empty', async () => {
+      const req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/admin/register',
+        body: {
+          username: 'admin1',
+          email: '',
+          password: 'securepassword',
+        },
+      });
 
-  it('create an admin should throw an error if username is empty', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        username: 'admin1',
-        email: '',
-        password: 'securepassword',
-      },
+      await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
     });
 
-    await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
-  });
+    it('create an admin should throw an error if password is empty', async () => {
+      const req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/admin/register',
+        body: {
+          username: 'admin1',
+          email: 'admin1@example.com',
+          password: '',
+        },
+      });
 
-  it('create an admin should throw an error if password is empty', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        username: 'admin1',
-        email: 'admin1@example.com',
-        password: '',
-      },
+      await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
     });
 
-    await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
-  });
+    it('create an admin should throw an error if email is invalid', async () => {
+      const req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/admin/register/',
+        body: {
+          username: 'admin1',
+          email: 'admin1example.com',
+          password: 'securepassword',
+        },
+      });
 
-  it('create an admin should throw an error if email is invalid', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        username: 'admin1',
-        email: 'admin1example.com',
-        password: 'securepassword',
-      },
+      await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
+    });
     });
 
-    await expect(controller.createAdmin(req)).rejects.toThrow(ValidationError);
-  });
 
+    describe('LoginAdmin', () => {
 
-  it('should login an admin successfully with username and password', async () => {
+      it('should login an admin successfully with username and password', async () => {
 
-    const mockAdminWithToken = {
-      username: 'admin1',
-      email: 'admin1@example.com',
-      token: 'mocked-jwt-token',
-      password: undefined,
-    };
-    (AdminAuthService.prototype.loginAdmin as jest.Mock).mockResolvedValue(mockAdminWithToken);
+        const mockAdminWithToken = {
+          username: 'admin1',
+          email: 'admin1@example.com',
+          token: 'mocked-jwt-token',
+          password: undefined,
+        };
+        (AdminAuthService.prototype.loginAdmin as jest.Mock).mockResolvedValue(mockAdminWithToken);
 
-    const reqLogin = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        emailOrUsername: 'admin1',
-        password: 'securepassword',
-      },
+        const reqLogin = httpMocks.createRequest({
+          method: 'POST',
+          url: '/admin/login',
+          body: {
+            emailOrUsername: 'admin1',
+            password: 'securepassword',
+          },
+        });
+
+        const response = await controller.loginAdmin(reqLogin);
+        expect(response).toEqual({ data: mockAdminWithToken });
+        expect(AdminAuthService.prototype.loginAdmin).toHaveBeenCalledWith('admin1', 'securepassword');
+      });
+
+      it('should login an admin successfully with email and password', async () => {
+
+        const mockAdminWithToken = {
+          username: 'admin1',
+          email: 'admin1@example.com',
+          token: 'mocked-jwt-token',
+          password: undefined,
+        };
+        (AdminAuthService.prototype.loginAdmin as jest.Mock).mockResolvedValue(mockAdminWithToken);
+
+        const reqLogin = httpMocks.createRequest({
+          method: 'POST',
+          url: '/admin/login',
+          body: {
+            emailOrUsername: 'admin1@example.com',
+            password: 'securepassword',
+          },
+        });
+
+        const response = await controller.loginAdmin(reqLogin);
+        expect(response).toEqual({ data: mockAdminWithToken });
+        expect(AdminAuthService.prototype.loginAdmin).toHaveBeenCalledWith('admin1@example.com', 'securepassword');
+      });
+
+      it('login should throw an error if emailOrUsername is empty', async () => {
+        const req = httpMocks.createRequest({
+          method: 'POST',
+          url: '/admin/login',
+          body: {
+            emailOrUsername: '',
+            password: 'securepassword',
+          },
+        });
+
+        await expect(controller.loginAdmin(req)).rejects.toThrow(ValidationError);
+      });
+
+      it('login should throw an error if password is empty', async () => {
+        const req = httpMocks.createRequest({
+          method: 'POST',
+          url: '/admin/login',
+          body: {
+            emailOrUsername: 'admin1',
+            password: '',
+          },
+        });
+
+        await expect(controller.loginAdmin(req)).rejects.toThrow(ValidationError);
+      });
+
+      it('login should throw an error if emailOrUsername is invalid', async () => {
+        const req = httpMocks.createRequest({
+          method: 'POST',
+          url: '/admin/login',
+          body: {
+            emailOrUsername: 'admin1@example',
+            password: 'securepassword',
+          },
+        });
+
+        await expect(controller.loginAdmin(req)).rejects.toThrow(ValidationError);
+      });
     });
-
-    const response = await controller.loginAdmin(reqLogin);
-    expect(response).toEqual({ data: mockAdminWithToken });
-    expect(AdminAuthService.prototype.loginAdmin).toHaveBeenCalledWith('admin1', 'securepassword');
   });
-
-  it('should login an admin successfully with email and password', async () => {
-
-    const mockAdminWithToken = {
-      username: 'admin1',
-      email: 'admin1@example.com',
-      token: 'mocked-jwt-token',
-      password: undefined,
-    };
-    (AdminAuthService.prototype.loginAdmin as jest.Mock).mockResolvedValue(mockAdminWithToken);
-
-    const reqLogin = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/create',
-      body: {
-        emailOrUsername: 'admin1@example.com',
-        password: 'securepassword',
-      },
-    });
-
-    const response = await controller.loginAdmin(reqLogin);
-    expect(response).toEqual({ data: mockAdminWithToken });
-    expect(AdminAuthService.prototype.loginAdmin).toHaveBeenCalledWith('admin1@example.com', 'securepassword');
-  });
-
-  it('login should throw an error if emailOrUsername is empty', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/login',
-      body: {
-        emailOrUsername: '',
-        password: 'securepassword',
-      },
-    });
-
-    await expect(controller.loginAdmin(req)).rejects.toThrow(ValidationError);
-  });
-
-  it('login should throw an error if password is empty', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/login',
-      body: {
-        emailOrUsername: 'admin1',
-        password: '',
-      },
-    });
-
-    await expect(controller.loginAdmin(req)).rejects.toThrow(ValidationError);
-  });
-
-  it('login should throw an error if emailOrUsername is invalid', async () => {
-    const req = httpMocks.createRequest({
-      method: 'POST',
-      url: '/admin/login',
-      body: {
-        emailOrUsername: 'admin1@example',
-        password: 'securepassword',
-      },
-    });
-
-    await expect(controller.loginAdmin(req)).rejects.toThrow(ValidationError);
-  });
-});
