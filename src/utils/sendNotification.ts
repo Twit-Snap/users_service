529931e2394382d45fbb871848e6c1ee62c8ace1
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-16 22:57:51 -0300

    feat: sendPushNotification function

---
+++ src/utils/sendNotification.ts
@@ -0,0 +1,31 @@
+/* istanbul ignore file */
+export const sendPushNotification = async (
+  expoPushToken: string,
+  title: string,
+  body: string,
+  data: object = {},
+  sound: string = 'default'
+) => {
+  const message = {
+    to: expoPushToken,
+    sound: sound,
+    title: title,
+    body: body,
+    data: data
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      host: 'exp.host',
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(message)
+  }).then(response => {
+    if (response.ok) {
+      console.log('notification sent!');
+    }
+  });
+};
