eb2586ca5c9b102b439fd1a4770efa896dcb7c4c
Luis Escalante <lescalante@fi.uba.ar>
2024-11-23 17:41:44 -0300

    fix db connection

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -2,16 +2,16 @@ import express from 'express';
 import { UserService } from '../services/userService';
 
 const router = express.Router();
-const userService = new UserService();
+
 
 // Endpoint to get all interests
 router.get('/interests', async (req, res, next) => {
-    try {
-        const data = await userService.getAllInterests();
-        res.status(200).json(data);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const data = await new UserService().getAllInterests();
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
